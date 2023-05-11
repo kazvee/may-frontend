781491690f4acd9ff4c263cdce2b8b6a65733c7f
karvok <kayvee123@gmail.com>
2023-05-10 20:48:10 -0600

    Add Projects endpoint

---
+++ src/pages/api/projects.js
@@ -0,0 +1,16 @@
+export default function handler(req, res) {
+  res.status(200).json([
+    {
+      _id: 1,
+      name: 'Project 1',
+      description: 'Project 1 is my first project.',
+      imageUrl: 'https://placedog.net/280?random=1',
+    },
+    {
+      _id: 2,
+      name: 'Project 2',
+      description: 'Project 2 is my second project.',
+      imageUrl: 'https://placedog.net/280?random=2',
+    },
+  ]);
+}
