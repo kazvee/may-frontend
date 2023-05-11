ed287bc27e873a8cd59f038e18f7fd8a237430cf
karvok <kayvee123@gmail.com>
2023-05-10 21:20:45 -0600

    Add projects using SSR

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,3 +1,40 @@
-export default function ProjectsPage() {
-  return <h1>My Projects</h1>;
+import PageDescription from '@/components/PageDescription';
+import ProjectItem from '@/components/ProjectItem';
+
+export default function ProjectsPage({ projects }) {
+  return (
+    <section>
+      <PageDescription
+        title='Projects'
+        description='Here are some of my personal projects'
+      />
+
+      {projects.map((project) => (
+        <ProjectItem key={project._id} project={project} />
+      ))}
+    </section>
+  );
+}
+
+export async function getServerSideProps() {
+  let projects = [
+    {
+      _id: 1,
+      name: 'Project 1',
+      description: 'Project 1 is my first project.',
+      imageUrl: 'https://placedog.net/280?random=1',
+    },
+    {
+      _id: 2,
+      name: 'Project 2',
+      description: 'Project 2 is my second project.',
+      imageUrl: 'https://placedog.net/280?random=2',
+    },
+  ];
+
+  return {
+    props: {
+      projects,
+    },
+  };
 }
