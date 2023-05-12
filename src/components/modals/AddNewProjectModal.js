fc5178bf7fe5a8bc3a74bbf54f3aee338540ec75
karvok <kayvee123@gmail.com>
2023-05-11 20:40:10 -0600

    Add handler for submit new project

--- src/components/modals/AddNewProjectModal.js
+++ src/components/modals/AddNewProjectModal.js
@@ -1,4 +1,5 @@
 import {
+  Button,
   Dialog,
   DialogActions,
   DialogContent,
@@ -6,14 +7,31 @@ import {
 } from '@mui/material';
 import ProjectForm from '../forms/ProjectForm';
 
-export default function AddNewProjectModal({ open, onClose }) {
+export default function AddNewProjectModal({ open, onClose, onSubmit }) {
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>Adding a new project...</DialogTitle>
       <DialogContent>
-        <ProjectForm />
+        <ProjectForm onSubmit={onSubmit} />
       </DialogContent>
-      <DialogActions></DialogActions>
+      <DialogActions>
+        <Button
+          variant='contained'
+          color='error'
+          type='reset'
+          form='project-form'
+        >
+          Clear form
+        </Button>
+        <Button
+          variant='contained'
+          type='submit'
+          form='project-form'
+          onClick={onClose}
+        >
+          Add project
+        </Button>
+      </DialogActions>
     </Dialog>
   );
 }
